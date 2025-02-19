fd258904f9445622a13a8ab6db77c22a8f70a45a
cossack-done <ser90593696.zhuravlev@yandex.ru>
2025-02-19 23:13:35 +0300

    feat: add new build common

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,27 @@ import { defineConfig } from 'vite'
 // @ts-ignore
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-
 export default defineConfig({
   server:{
     port:3000,
   },
   resolve: {
     alias: {
+      // @ts-ignore
       "@":fileURLToPath(new URL('./src',import.meta.url)),
     },
-    extensions: ['.mjs', '.js', '.ts', '.json']  // Добавляем поддержку расширения .ts
+    extensions: ['.mjs', '.js', '.ts', '.json']
   },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'chkutils',
-      fileName: 'chkutils',
-      formats:['es']
+      formats:['es','cjs'],
+      fileName: (format) => {
+        if (format === 'es') return 'chkutils.esm.js';
+        if (format === 'cjs') return 'chkutils.common.js';
+        return 'chkutils.js';
+      },
     },
     rollupOptions: {
       // external: ['vue'],
