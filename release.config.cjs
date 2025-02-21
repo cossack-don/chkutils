59146dee581c0edd1ff2bdea47e849007ccc5b95
cossack-done <ser90593696.zhuravlev@yandex.ru>
2025-02-21 20:02:06 +0300

    fix: change config release

--- release.config.cjs
+++ release.config.cjs
@@ -5,12 +5,10 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     ["@semantic-release/github"],
     "@semantic-release/npm",
-    [
-      "@semantic-release/git",
-      {
+    ["@semantic-release/changelog", {"changelogFile": "CHANGELOG.md" }],
+    ["@semantic-release/git", {
         assets: ["package.json", "CHANGELOG.md"],
         message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+      }],
   ],
 };
