2512fcf493a16ce9ffb0d946f55dc79c7947825a
cossack-done <ser90593696.zhuravlev@yandex.ru>
2025-03-21 17:58:09 +0300

    change helper returnDefaultField

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,9 @@
 import { cloneDeep } from "./cloneDeep";
-import { unDefWithDefaultField } from "./unDefWithDefaultField";
+import { returnDefaultField } from "./returnDefaultField";
 import {toTypeString} from "./toTypeString";
 
 export const _h = {
-  unDefWithDefaultField,
+  returnDefaultField,
   cloneDeep,
   toTypeString
 }
